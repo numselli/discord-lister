84a856847082350be7634da3257179db1eec25de
ElectricReality <34076729+ElectricReality@users.noreply.github.com>
2019-07-30 20:02:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
+const https = require('https')
+
 module.exports = {
-     post: async (settings, data) => {
-        if(settings.discordbotsorg === "true" || settings.discordbotsgg === "true") {
-        
-        } else {
-          console.log("Nothing to post")
+     post: async (settings) => {
+        if(settings.discordbotsorg === true) {
+           if(!settings.discordbotsorg_token){
+             return console.log("discordbotsorg_token is not set!")
+           }
+           let data = JSON.stringify({
+             'server_count' : settings.servercount,
+             'shard_count' : settings.shardscount,
+           });
+           let options = {
+              hostname: 'discordbots.org/api',
+              port: 443,
+              path: `/bots/{bot.id}/stats`,
+              method: 'POST',
+              headers: {
+                 'Content-Type': 'application/json',
+                 'Content-Length': data.length,
+                 'Authorization': settings.discordbotsorg_token
+              }
+           }
+           const req = https.request(options, (res) => {
+              console.log(`statusCode: ${res.statusCode}`)
+                 res.on('data', (d) => {
+                    process.stdout.write(d)
+                 })
+           })
+           req.on('error', (error) => {
+              console.error(error)
+           })
+           req.write(data)
+           req.end()
+        },
+             
+        if(settings.discordbotsgg === true) {
+           
         }
      },
 
