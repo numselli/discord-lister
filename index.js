127bc3eee7e233407be05c586613b198a51c9e76
ElectricReality <34076729+ElectricReality@users.noreply.github.com>
2019-07-30 19:10:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+module.exports = {
+     post: async (settings, data) => {
+        if(settings.discordbotsorg === "true" || settings.discordbotsgg === "true") {
+        
+        } else {
+          console.log("Nothing to post")
+        }
+     },
 
+};
