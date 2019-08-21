1c53230ac6292c18e417c923a120ee390ec2e3ea
ElectricReality <34076729+ElectricReality@users.noreply.github.com>
2019-08-21 20:09:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,82 +1,108 @@
 const https = require('https')
 
 module.exports = {
-     post: async (settings) => {
-          if(settings.discordbotsorg.enabled === true) {
-           if(!settings.discordbotsorg.token){
-             return console.log("[discordbots.org] Token is not set!")
-           }
-           let data = JSON.stringify({
-             'server_count' : settings.servercount || 0,
-             'shard_count' : settings.shardscount || 0,
-             'shard_id': settings.shardsid || 0
-           });
-           let options = {
-              hostname: 'discordbots.org',
-              port: 443,
-              path: `/api/bots/stats`,
-              method: 'POST',
-              headers: {
-                 'Content-Type': 'application/json',
-                 'Content-Length': data.length,
-                 'Authorization': settings.discordbotsorg.token
-              }
-           }
-           
-           
-           const req = https.request(options, (res) => {
-                if(res.statusCode === 200){
-                     console.log('discordbots.org post success!')
-                } else {
-                  console.log(`[discordbots.org] An error occured: ${res.statusCode}, ${res.statusMessage}`)
-                  console.log('[discordbots.org]This may be a cause of a malformed API Token.')
-                }
-                 
-           })
-           
-           req.on('error', (error) => {
-              console.log(error)
-           })
-           req.write(data)
-           req.end()
+  post: async(settings) => {
+    if (settings.listings.discordbotsorg) {
+      if (!settings.listings.discordbotsorg) {
+        console.log("[discordbots.org] Token not set!")
+      } else {
+        let data = JSON.stringify({
+          'server_count': settings.servercount || 0,
+          'shard_count': settings.shardscount || 0,
+          'shard_id': settings.shardsid || 0
+        });
+        let options = {
+          hostname: 'discordbots.org',
+          port: 443,
+          path: `/api/bots/stats`,
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': data.length,
+            'Authorization': settings.discordbotsorg
+          }
         }
-        if(settings.discordbotsgg.enabled === true) {
-           if(!settings.discordbotsgg.token){
-             return console.log("[discord.bots.gg] Token is not set!")
-           }
-           let data = JSON.stringify({
-             'guildCount' : settings.servercount || 0,
-             'shardCount' : settings.shardscount || 0,
-             'shardId': settings.shardsid || 0
-           });
-           let options = {
-              hostname: 'discord.bots.gg',
-              port: 443,
-              path: `/api/v1/bots/${settings.discordbotsgg.client_id}/stats`,
-              method: 'POST',
-              headers: {
-                 'Content-Type': 'application/json',
-                 'Content-Length': data.length,
-                 'Authorization': settings.discordbotsgg.token
-              }
-           }
-           
-           
-           const req = https.request(options, (res) => {
-                if(res.statusCode === 200){
-                     console.log('discord.bots.gg post success!')
-                } else {
-                  console.log(`[discord.bots.gg] An error occured: ${res.statusCode}, ${res.statusMessage}`)
-                  console.log('[discord.bots.gg] This may be a cause of a malformed API Token or Invalid Client ID.')
-                }
-                 
-           })
-           
-           req.on('error', (error) => {
-              console.log(error)
-           })
-           req.write(data)
-           req.end()
+        const req = https.request(options, (res) => {
+          if (res.statusCode === 200) {
+            console.log('[discordbots.org] Post success!')
+          } else {
+            console.log(`[discordbots.org] An error occured: ${res.statusCode}, ${res.statusMessage}`)
+          }
+        })
+        req.on('error', (error) => {
+          console.log(error)
+        })
+        req.write(data)
+        req.end()
+      }
+    }
+
+    if (settings.listings.discordbotsgg) {
+      if (!settings.listings.discordbotsgg) {
+        console.log("[discord.bots.gg] Token not set!")
+      } else {
+        let data = JSON.stringify({
+          'server_count': settings.servercount || 0,
+          'shard_count': settings.shardscount || 0,
+          'shard_id': settings.shardsid || 0
+        });
+        let options = {
+          hostname: 'discord.bots.gg',
+          port: 443,
+          path: `/api/v1/bots/${settings.clientid}/stats`,
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': data.length,
+            'Authorization': settings.discordbotsgg
+          }
         }
-     }
+        const req = https.request(options, (res) => {
+          if (res.statusCode === 200) {
+            console.log('[discord.bots.gg] Post success!')
+          } else {
+            console.log(`[discord.bots.gg] An error occured: ${res.statusCode}, ${res.statusMessage}`)
+          }
+        })
+        req.on('error', (error) => {
+          console.log(error)
+        })
+        req.write(data)
+        req.end()
+      }
+    }
+    if (settings.listings.discordboats) {
+      if (!settings.listings.discordboats) {
+        console.log("[discord.boats] Token not set!")
+      } else {
+        let data = JSON.stringify({
+          'server_count': settings.servercount || 0,
+        });
+        let options = {
+          hostname: 'discord.boats',
+          port: 443,
+          path: `discord.boats/api/v2/bot/${settings.clientid}`,
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': data.length,
+            'Authorization': settings.discordboats
+          }
+        }
+        const req = https.request(options, (res) => {
+          if (res.statusCode === 200) {
+            console.log('[discord.boats] Post success!')
+          } else {
+            console.log(`[discord.boats] An error occured: ${res.statusCode}, ${res.statusMessage}`)
+          }
+        })
+        req.on('error', (error) => {
+          console.log(error)
+        })
+        req.write(data)
+        req.end()
+      }
+    }
+
+  }
 };
