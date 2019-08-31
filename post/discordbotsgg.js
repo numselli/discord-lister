bd7c904ea61a4e2af2d5e2e77d31bac959333227
ElectricReality <34076729+ElectricReality@users.noreply.github.com>
2019-08-31 19:22:24 +0800

    Create discordbotsgg.js

---
+++ post/discordbotsgg.js
@@ -0,0 +1,42 @@
+const https = require('https')
+
+module.exports = settings => {
+
+  if (!settings.listings.discordbotsgg) {
+    return console.log("[discord.bots.gg] Token not set!")
+  }
+  
+  let data = JSON.stringify({
+    'server_count': settings.servercount || 0,
+    'shard_count': settings.shardscount || 0,
+    'shard_id': settings.shardsid || 0
+  });
+  
+  let options = {
+    hostname: 'discord.bots.gg',
+    port: 443,
+    path: `/api/v1/bots/${settings.clientid}/stats`,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': data.length,
+      'Authorization': settings.listings.discordbotsgg
+    }
+  }
+  
+  const req = https.request(options, (res) => {
+    if (res.statusCode === 200) {
+      console.log('[discord.bots.gg] Post success!')
+    } else {
+      console.log(`[discord.bots.gg] An error occured: ${res.statusCode}, ${res.statusMessage}`)
+    }
+  })
+  
+  req.on('error', (error) => {
+    console.log(error)
+  })
+  
+  req.write(data)
+  
+  req.end()
+};
