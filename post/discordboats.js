8fe80ab32de4c449666c0eefec252ea692744387
ElectricReality <34076729+ElectricReality@users.noreply.github.com>
2019-08-31 19:27:57 +0800

    Create discordboats.js

---
+++ post/discordboats.js
@@ -0,0 +1,40 @@
+const https = require('https')
+
+module.exports = settings => {
+
+  if (!settings.listings.discordboats) {
+    return console.log("[discord.boats] Token not set!")
+  }
+  
+  let data = JSON.stringify({
+    'server_count': settings.servercount || 0,
+  });
+  let options = {
+    hostname: 'discord.boats',
+    port: 443,
+    path: `/api/v2/bot/${settings.clientid}`,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': data.length,
+      'Authorization': settings.listings.discordboats
+    }
+  }
+  
+  const req = https.request(options, (res) => {
+    if (res.statusCode === 200) {
+      console.log('[discord.boats] Post success!')
+    } else {
+      console.log(`[discord.boats] An error occured: ${res.statusCode}, ${res.statusMessage}`)
+    }
+  })
+  
+  req.on('error', (error) => {
+    console.log(error)
+  })
+  
+  req.write(data)
+  
+  req.end()
+        
+};
