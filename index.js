9849d322c1fd83f29cafd9f420a0be67c2835ca8
ElectricReality <34076729+ElectricReality@users.noreply.github.com>
2019-08-31 22:12:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ module.exports = {
     if(settings.discordbotsorg){
       require("./post/discordbotsorg")(settings);
     }
+    if(settings.discordbotsgg){
+      require("./post/discordbotsgg")(settings);
+    }
+    if(settings.discordboats){
+      require("./post/discordboats")(settings);
+    }
+    if(settings.botsondiscord){
+      require("./post/botsondiscord")(settings);
+    }
+    if(settings.botsfordiscord){
+      require("./post/botsfordiscord")(settings);
+    }
+    if(settings.botlistspace){
+      require("./post/botlistspace")(settings);
+    }
+    if(settings.divinediscordbots){
+      require("./post/divinediscordbots")(settings);
+    }
   }
 };
         
