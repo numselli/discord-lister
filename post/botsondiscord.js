b7744af1dec9859a3da869e2fe9bc71f257ed9b9
ElectricReality <34076729+ElectricReality@users.noreply.github.com>
2019-08-31 23:12:20 +0800

    Update botsondiscord.js

--- post/botsondiscord.js
+++ post/botsondiscord.js
@@ -22,7 +22,8 @@ module.exports = settings => {
     if (res.statusCode === 200) {
       console.log('[bots.ondiscord.xyz] Post success!')
     } else {
-      console.log(`[bots.ondiscord.xyz] An error occured: ${res.statusCode}, ${res.statusMessage} ${JSON.parse(res)}`)
+      console.log(res)
+      console.log(`[bots.ondiscord.xyz] An error occured: ${res.statusCode}, ${res.statusMessage}`)
     }
   })
   req.on('error', (error) => {
