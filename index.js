e19f05732331f168736fcceae3e0b134c5abe039
numselli <58607232+numselli@users.noreply.github.com>
2021-06-10 21:48:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const fs = require("fs/promises")
-const files = await fs.readdir(`${__dirname}/post`);
 
 module.exports = {
 	post: async (settings) => {
 		if (!settings.clientid) return console.log('clientid NOT DEFINED!')
+		const files = await fs.readdir(`${__dirname}/post`);
 		files.forEach(list => require(`./post/${list}`)(settings))
 	}
 };
